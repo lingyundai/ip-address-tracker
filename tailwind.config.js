3315d91a3194b31479d0ae58eb625e5a7e183836
lingyundai <lingyundaiseven@gmail.com>
2023-08-15 17:48:04 -0400

    installed tailwind, added background

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'desktop-header-image': "url('../src/assets/images/pattern-bg-desktop.png')",
+        'mobile-header-image': "url('/src/assets/images/pattern-bg-mobile.png')",
+      },
+      screens: {
+        'sm': [
+          {'min': '0px', 'max': '500px'}
+        ],
+        'lg': [
+          {'min': '500px'}
+        ]
+      },
+    },
+  },
+  plugins: [],
+}
+
