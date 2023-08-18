4f08589eb1d77306ea4aa7abb9b6ed3947a1b5a3
lingyundai <lingyundaiseven@gmail.com>
2023-08-17 23:42:40 -0400

    minor changes

--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -1,10 +1,25 @@
 function Tracker () {
     return (
-        <div class="flex">
+        <div class="flex text-center">
             <div class="w-full h-64 bg-cover lg:bg-desktop-header-image sm:bg-mobile-header-image">
-                <header class="text-center pt-8">
+                <header class="pt-8">
                     <h1 class="text-2xl font-rubik font-bold tracking-wide text-white">IP Address Tracker</h1>
                 </header>
+                <form class="relative">
+                    <input
+                        type="search" 
+                        id="default-search"
+                        class="w-3/4 p-3 mt-6 rounded-lg" 
+                        placeholder="Search for any IP address or domain" 
+                        required
+                    >
+                    </input>
+                    <button class="w-6 h-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="10" height="14" class="">
+                            <path fill="#000000" stroke="#FFF" stroke-width="3" d="M2 1l6 6-6 6" />
+                        </svg>
+                    </button>
+                </form>
             </div>
         </div>
     )
