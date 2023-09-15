321f52d61db038f4ca760fa5ace51a84557f30b2
lingyundai <lingyundaiseven@gmail.com>
2023-09-15 00:10:01 -0400

    get long and lat for map

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import TrackerContainer from "./features/tracker/TrackerContainer";
+import { useRef } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 function App() {
+  const mapRef = useRef();
+
   return (
     <div className="App">
-      <TrackerContainer />
+    <TrackerContainer />
     </div>
   );
 }
